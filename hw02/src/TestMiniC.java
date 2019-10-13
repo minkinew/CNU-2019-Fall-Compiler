ae148227015324eab0ad4156add196eb342c8576
minkinew <44183457+minkinew@users.noreply.github.com>
2019-10-13 21:21:13 +0900

    MiniC

---
+++ hw02/src/TestMiniC.java
@@ -0,0 +1,11 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class TestMiniC {
+    public static void main(String[] args) throws  Exception{
+        MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("./test.c"));
+        CommonTokenStream tokens = new CommonTokenStream( lexer );
+        MiniCParser parser = new MiniCParser(tokens);
+        ParseTree tree = parser.program();
+    }
+}
---
+++ hw02/test.c
@@ -0,0 +1,10 @@
+int a;
+int b[10];
+int main(){
+    test(5);
+    return 0;
+}
+int test(int n){
+    int c;
+    return c;
+}
