2312cbe4083a7bdf37e219b051123d35e7d83ca4
minkinew <44183457+minkinew@users.noreply.github.com>
2019-10-23 11:24:36 +0900

    Add Java bytecode mnemonic code using jasmin

---
+++ hw04/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+    public Test() {
+    }
+
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] var0) {
+        int var = 33;
+        System.out.println(add(1, var));
+    }
+}
